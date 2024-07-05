c2efd72e77825cb4c4f35ce83220d6bdc38c3de5
tsawant2727 <tukaram.sawant@patdelanalytics.com>
2024-07-05 08:53:29 +0530

    added useeffect

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
 import { useRouter } from 'next/router';
 import useStore from '../lib/store';
 import styles from './StartScreen.module.css';
